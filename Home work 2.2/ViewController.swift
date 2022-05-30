7b5ca9ebb5c86989dde78b517756c5ac208b76d1
Tamirlan <8gtz8@vk.com>
2022-05-30 20:21:07 +0300

    Initial Commit

---
+++ Home work 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2.2
+//
+//  Created by Tamirlan on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
